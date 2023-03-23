80254f16930a8a594bb447cba2a96788a1e7e018
szczodrowsky <116168292+szczodrowsky@users.noreply.github.com>
2023-03-23 16:21:18 +0100

    Add files via upload
    Co-authored-by: Mateusz Makowiecki <m4kowiecki@gmail.com>

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A1 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // A1
+            // 
+            this.A1.BackColor = System.Drawing.Color.DarkRed;
+            this.A1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.A1.Location = new System.Drawing.Point(12, 12);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(99, 99);
+            this.A1.TabIndex = 1;
+            this.A1.TabStop = false;
+            this.A1.UseVisualStyleBackColor = false;
+            this.A1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // C3
+            // 
+            this.C3.BackColor = System.Drawing.Color.DarkRed;
+            this.C3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.C3.Location = new System.Drawing.Point(222, 222);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(99, 99);
+            this.C3.TabIndex = 9;
+            this.C3.TabStop = false;
+            this.C3.UseVisualStyleBackColor = false;
+            this.C3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // C2
+            // 
+            this.C2.BackColor = System.Drawing.Color.DarkRed;
+            this.C2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.C2.Location = new System.Drawing.Point(117, 222);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(99, 99);
+            this.C2.TabIndex = 8;
+            this.C2.TabStop = false;
+            this.C2.UseVisualStyleBackColor = false;
+            this.C2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // C1
+            // 
+            this.C1.BackColor = System.Drawing.Color.DarkRed;
+            this.C1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.C1.Location = new System.Drawing.Point(12, 222);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(99, 99);
+            this.C1.TabIndex = 7;
+            this.C1.TabStop = false;
+            this.C1.UseVisualStyleBackColor = false;
+            this.C1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // B3
+            // 
+            this.B3.BackColor = System.Drawing.Color.DarkRed;
+            this.B3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.B3.Location = new System.Drawing.Point(222, 117);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(99, 99);
+            this.B3.TabIndex = 6;
+            this.B3.TabStop = false;
+            this.B3.UseVisualStyleBackColor = false;
+            this.B3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // B2
+            // 
+            this.B2.BackColor = System.Drawing.Color.DarkRed;
+            this.B2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.B2.Location = new System.Drawing.Point(117, 117);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(99, 99);
+            this.B2.TabIndex = 5;
+            this.B2.TabStop = false;
+            this.B2.UseVisualStyleBackColor = false;
+            this.B2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // B1
+            // 
+            this.B1.BackColor = System.Drawing.Color.DarkRed;
+            this.B1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.B1.Location = new System.Drawing.Point(12, 117);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(99, 99);
+            this.B1.TabIndex = 4;
+            this.B1.TabStop = false;
+            this.B1.UseVisualStyleBackColor = false;
+            this.B1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // A3
+            // 
+            this.A3.BackColor = System.Drawing.Color.DarkRed;
+            this.A3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.A3.Location = new System.Drawing.Point(222, 12);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(99, 99);
+            this.A3.TabIndex = 3;
+            this.A3.TabStop = false;
+            this.A3.UseVisualStyleBackColor = false;
+            this.A3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // A2
+            // 
+            this.A2.BackColor = System.Drawing.Color.DarkRed;
+            this.A2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.A2.Location = new System.Drawing.Point(117, 12);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(99, 99);
+            this.A2.TabIndex = 2;
+            this.A2.TabStop = false;
+            this.A2.UseVisualStyleBackColor = false;
+            this.A2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(336, 337);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A1);
+            this.MaximumSize = new System.Drawing.Size(352, 376);
+            this.MinimumSize = new System.Drawing.Size(352, 376);
+            this.Name = "Form1";
+            this.Text = "Kółko i krzyżyk";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button A2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        bool turn = true; 
+        int turn_count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void button_Click(object sender, EventArgs e) //kliknięcie przycisku
+        {
+            Button b = (Button)sender;
+            if (turn)
+                b.Text = "X";
+            else
+                b.Text = "O";
+
+            turn = !turn;
+         
+            b.Enabled = false;
+            turn_count++;
+            checkForWinner();
+
+        }
+        private void disableButtons() //wyłączanie przycisków
+        {
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+                }
+            }
+            catch { }
+        }
+
+        private void newGame() //reset gry
+        {
+            turn = true;
+            turn_count = 0;
+
+            foreach (Control c in Controls)
+            {
+                try
+                {
+                    Button b = (Button)c;
+                    b.Enabled = true;
+                    b.Text = "";
+                }
+                catch { }
+            }
+        }
+        private void draw() // remis
+        {
+            disableButtons();
+            
+            MessageBox.Show("Remis");
+            newGame();
+        }
+        private void checkForWinner() //sprawdzenie wygranej
+        {
+            bool there_is_a_winner = false;
+
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A1.Enabled))
+                there_is_a_winner = true;
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled))
+                there_is_a_winner = true;
+            else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled))
+                there_is_a_winner = true;
+            else if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled))
+                there_is_a_winner = true;
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled))
+                there_is_a_winner = true;
+            else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!A3.Enabled))
+                there_is_a_winner = true;
+
+            else if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled))
+                there_is_a_winner = true;
+            else if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && (!C1.Enabled))
+                there_is_a_winner = true;
+            else if (turn_count == 9)
+                draw();
+                
+            if (there_is_a_winner) //wygrana
+            {
+                disableButtons();
+                String winner = "";
+                if (turn)
+                {
+                    winner = "O";
+
+                }
+                else
+                {
+                    winner = "X";
+
+                }
+                MessageBox.Show(winner + " Wygrywa");
+                newGame();
+            }
+        }
+    }
+}
+
